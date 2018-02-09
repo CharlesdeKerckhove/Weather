d1a534170b32331d0dbe13c9411a27beab2b9279
CrazyC008 <DEKC1_16@uni.worc.ac.uk>
2018-02-09 15:22:25 +0000

    recreate file system

---
+++ js/index.js
@@ -0,0 +1,5 @@
+Backendless.initApp("AC29438F-295C-417A-FF51-2F117FAA4300", "2081B5D2-2E7C-6F96-FFD9-590A4D57A800");
+$(document).on("pageshow", "#todoPage", onPageShow);
+function onPageShow() {
+console.log("page shown");
+}
