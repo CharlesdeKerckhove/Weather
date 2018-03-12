c533be7d5b058d52699bd3ed9c5d3d6c8f1c3bca
CrazyC008 <DEKC1_16@uni.worc.ac.uk>
2018-03-12 15:36:05 +0000

    weather

--- js/index.js
+++ js/index.js
@@ -1,28 +1,24 @@
-Backendless.initApp("AC29438F-295C-417A-FF51-2F117FAA4300", "2081B5D2-2E7C-6F96-FFD9-590A4D57A800");
+//create and send XHR request
+function makeRequest() {
+     console.log("make");
+xhr = new XMLHttpRequest();
+xhr.onreadystatechange = responseMethod;
+xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Worcester,England' + '&appid=' + '9df93abfcfe54178c78910bf2dafac74', true);
+xhr.send();
+    console.log("sent");
 
-$(document).on("pageshow","#todoPage", onPageShow);
-
-function onPageShow() {
-console.log("page shown");
 }
-
-Backendless.Data.of("Tasks").find().then(processResults).catch(error);
-
-function processResults(tasks) {
- //display the first task in an array of tasks.
-    alert(tasks[1].Task)
- //wipe the list clean
-$('#taskList').empty();
-    console.log("list wiped");
- //add each tasks
-for (var i = 0; i < tasks.length; i++) {
-    $('#taskList').append("<li>" + tasks[i].Task+"</li>");
-    console.log("repeat add");
+//Handle XHR response – Callback Function
+function responseMethod() {
+     console.log("response");
+    if (xhr.readyState == 4) { // Ready
+        console.log("4");
+        if (xhr.status == 200) { // HTTP OK
+        console.log("200");
+            alert(xhr.responseText); //Assuming Text else responseXML
+        }
+        else {
+        alert('There was a problem with the request.');
+        }
+    }
 }
-}
-
-function error(err) {
- alert(err);
-}
-
-
